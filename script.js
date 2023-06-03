1c94708c3d480a5ac19c11377b51b0a50ef41fa3
Goph <tealeverywhere@gmail.com>
2023-06-02 21:11:42 -1000

    object parsing and drawing agogo

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ async function getWeatherForecast(){
   locationCity = "London"
   const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=522b8e29714a4227bbe50305231104&q=${locationCity}&aqi=no&days=3`);
   const jsonData = await response.json();
-  // console.log(jsonData);
-  // console.log(jsonData.location);
-  // console.log(jsonData.location.region);
+
+  console.log(jsonData);
+
   return jsonData;
 }
 
@@ -30,21 +30,60 @@ function processJSON(jsonData){
 
 function processForecastJSON(jsonData){
   console.log("3-day Forecast");
-  console.log(jsonData);
-  console.log(jsonData.location);
-  console.log(jsonData.location.region);
-  console.log(jsonData.forecast.forecastday)
-  console.log(jsonData.forecast.forecastday[0].day.maxtemp_c)
-  console.log(jsonData.forecast.forecastday[1])
-  console.log(jsonData.forecast.forecastday[2])
-  console.log("~~~~~~~~~~~End Forecast~~~~~~~~~~");
+
+  const {name, region} = jsonData.location;
+
+  const {forecastday} = jsonData.forecast;
+
+  const processed = {
+    name,
+    region,
+    forecastday
+  };
+  // console.log(jsonData);
+  // console.log(jsonData.location);
+  // console.log(jsonData.location.region);
+  // console.log(jsonData.forecast.forecastday)
+  // console.log(jsonData.forecast.forecastday[0].day.maxtemp_c)
+  // console.log(jsonData.forecast.forecastday[1])
+  // console.log(jsonData.forecast.forecastday[2])
+  console.log(processed);
+  return processed;
 }
 
 async function main(){
-  let blah = await getWeather();
-  processJSON(blah);
+  // let blah = await getWeather();
+  // processJSON(blah);
   blah = await getWeatherForecast();
   processForecastJSON(blah);
 }
 
+async function display(){
+  blah = await getWeatherForecast();
+  forecast = processForecastJSON(blah);
+  let location = forecast.name;
+  
+  let locationDiv = document.querySelector("#day1").querySelector(".location");
+  locationDiv.innerHTML = location;
+  console.log(locationDiv);
+
+  let maxtemp_c = forecast.forecastday[0].day.maxtemp_c;
+  let maxtemp_cDiv = document.querySelector("#day1").querySelector(".maxtemp_c");
+  maxtemp_cDiv.innerHTML = maxtemp_c;
+
+  let mintemp_c = forecast.forecastday[0].day.mintemp_c;
+  let mintemp_cDiv = document.querySelector("#day1").querySelector(".mintemp_c");
+  mintemp_cDiv.innerHTML = mintemp_c;
+
+  let maxtemp_f = forecast.forecastday[0].day.maxtemp_f;
+  let maxtemp_fDiv = document.querySelector("#day1").querySelector(".maxtemp_f");
+  maxtemp_fDiv.innerHTML = maxtemp_f;
+
+  let mintemp_f = forecast.forecastday[0].day.mintemp_f;
+  let mintemp_fDiv = document.querySelector("#day1").querySelector(".mintemp_f");
+  mintemp_fDiv.innerHTML = mintemp_f;
+
+}
+
 main();
+display();
